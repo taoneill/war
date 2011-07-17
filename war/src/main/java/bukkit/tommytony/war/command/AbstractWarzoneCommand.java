5565024a7c261615000c2b852bc5ca4e8d79e2e6
Tim Düsterhus <timwolla@arcor.de>
2011-07-17 12:56:31 +0200

    Some more change

--- war/src/main/java/bukkit/tommytony/war/command/AbstractWarzoneCommand.java
+++ war/src/main/java/bukkit/tommytony/war/command/AbstractWarzoneCommand.java
@@ -1,13 +1,16 @@
 package bukkit.tommytony.war.command;
 
+import org.bukkit.command.CommandSender;
+
 import com.tommytony.war.Warzone;
 
-public abstract class AbstractWarzoneCommand {
-
+public abstract class AbstractWarzoneCommand extends AbstractWarCommand {
+	protected int parameterCount = 0;
 	protected Warzone zone = null;
 	public AbstractWarzoneCommand(CommandSender sender, String[] args) {
 		super(sender, args);
 
+		// TODO: Find the warzone either from name (support console too) or from location
 	}
 
 
