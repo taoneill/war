554335b6bdbaba8ad166214e1b05c506373b58de
Tim Düsterhus <timwolla@arcor.de>
2011-07-17 18:28:32 +0200

    Half implementation of /teams

--- war/src/main/java/bukkit/tommytony/war/command/AbstractWarzoneCommand.java
+++ war/src/main/java/bukkit/tommytony/war/command/AbstractWarzoneCommand.java
@@ -1,19 +1,41 @@
 package bukkit.tommytony.war.command;
 
+import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
+import bukkit.tommytony.war.War;
 import bukkit.tommytony.war.WarCommandHandler;
 
 import com.tommytony.war.Warzone;
+import com.tommytony.war.ZoneLobby;
 
 public abstract class AbstractWarzoneCommand extends AbstractWarCommand {
-	protected int parameterCount = 0;
 	protected Warzone zone = null;
 	public AbstractWarzoneCommand(WarCommandHandler handler, CommandSender sender, String[] args) {
 		super(handler, sender, args);
-
-		// TODO: Find the warzone either from name (support console too) or from location
 	}
 
+	public Warzone getWarzoneFromLocation(Player player) {
+		return this.getWarzoneFromLocation(player.getLocation());
+	}
 
+	public Warzone getWarzoneFromLocation(Location location) {
+		Warzone zone = War.war.warzone(location);
+		if (zone == null) {
+			ZoneLobby lobby = War.war.lobby(location);
+			if (lobby == null) return null;
+			zone = lobby.getZone();
+		}
+		return zone;
+	}
+
+	public Warzone getWarzoneFromName(String name) {
+		for (Warzone zone : War.war.getWarzones()) {
+			if (zone.getName().toLowerCase().equals(name.toLowerCase())) {
+				return zone;
+			}
+		}
+		return null;
+	}
 }
---
+++ war/src/main/java/bukkit/tommytony/war/command/TeamsCommand.java
@@ -0,0 +1,31 @@
+package bukkit.tommytony.war.command;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import bukkit.tommytony.war.WarCommandHandler;
+
+import com.tommytony.war.Warzone;
+
+public class TeamsCommand extends AbstractWarzoneCommand {
+	public TeamsCommand(WarCommandHandler handler, CommandSender sender, String[] args) {
+		super(handler, sender, args);
+	}
+
+	public boolean handle() {
+		Warzone zone;
+		if (this.args.length == 1) {
+			zone = this.getWarzoneFromName(this.args[0]);
+		} else {
+			if (!(this.sender instanceof Player)) {
+				return false;
+			}
+			zone = this.getWarzoneFromLocation((Player) this.sender);
+		}
+		if (zone == null) {
+			return false;
+		}
+//		zone.getTeams();
+		return true;
+	}
+}
