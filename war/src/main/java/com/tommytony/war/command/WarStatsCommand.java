62284c3146b21ca7f00c1cfa5a5470b7a327022e
grinning <grin@tommytony.com>
2013-05-04 00:01:25 -0400

    Doing /warstats now returns your own stats

--- war/src/main/java/com/tommytony/war/command/WarStatsCommand.java
+++ war/src/main/java/com/tommytony/war/command/WarStatsCommand.java
@@ -2,6 +2,7 @@ package com.tommytony.war.command;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import com.tommytony.war.War;
 import com.tommytony.war.utility.PlayerStatTracker;
@@ -16,7 +17,17 @@ public class WarStatsCommand extends AbstractWarCommand {
 	public boolean handle() {
 		//we can do this if we are not in game :D
 		if(this.args.length == 0) {
-			return false;
+			if(!(this.getSender() instanceof Player)) {
+				this.badMsg("You must be ingame to use this command");
+				return true;
+			} 
+			PlayerStatTracker stat = PlayerStatTracker.getStats(this.getSender().getName());
+			this.msg("Your stats " + ChatColor.GOLD + "\n"
+					+ ChatColor.WHITE + "Kills: " + ChatColor.DARK_PURPLE + stat.getKills() + "\n"
+					+ ChatColor.WHITE + "Deaths: " + ChatColor.DARK_PURPLE + stat.getDeaths() + "\n"
+				    + ChatColor.WHITE + "Wins: " + ChatColor.DARK_PURPLE + stat.getWins() + "\n"
+				    + ChatColor.WHITE + "Losses: " + ChatColor.DARK_PURPLE + stat.getLosses());
+			return true;
 		} else if(this.args.length == 1) {
 			//they want every stat for a certain player
 			PlayerStatTracker stat = PlayerStatTracker.getStats(args[0]);  //we shall multithread this if it gets laggy
