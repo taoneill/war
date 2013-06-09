21688ba4d000a3067793b2be50c7ffdc4476ed08
cmastudios <admin@cmastudios.me>
2013-06-08 22:55:35 -0500

    Create zones with WorldEdit cuboid selections
    The WorldEdit API is utilized to allow the use of WorldEdit selections
    when creating or modifying a warzone. If the current player has a
    WorldEdit selection when the /setzone command is used, this selection will
    be used for the zone's corners.

--- war/src/main/java/com/tommytony/war/command/SetZoneCommand.java
+++ war/src/main/java/com/tommytony/war/command/SetZoneCommand.java
@@ -1,7 +1,10 @@
 package com.tommytony.war.command;
 
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldedit.bukkit.selections.Selection;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.Location;
 
 import com.tommytony.war.War;
 
@@ -26,6 +29,18 @@ public class SetZoneCommand extends AbstractZoneMakerCommand {
 		} else if (this.args.length > 2) {
 			return false;
 		} else if (this.args.length == 1) {
+			if (War.war.getServer().getPluginManager().isPluginEnabled("WorldEdit")) {
+				WorldEditPlugin worldEdit = (WorldEditPlugin) War.war.getServer().getPluginManager().getPlugin("WorldEdit");
+				Selection selection = worldEdit.getSelection(player);
+				if (selection != null) {
+					Location min = selection.getMinimumPoint();
+					Location max = selection.getMaximumPoint();
+					ZoneSetter setter = new ZoneSetter(player, this.args[0]);
+					setter.placeCorner1(min.getBlock());
+					setter.placeCorner2(max.getBlock());
+					return true;
+				}
+			}
 			War.war.addWandBearer(player, this.args[0]);
 		} else if (this.args.length == 2) {
 			if (!this.args[1].equals("southeast") && !this.args[1].equals("northwest") && !this.args[1].equals("se") && !this.args[1].equals("nw") && !this.args[1].equals("corner1") && !this.args[1].equals("corner2") && !this.args[1].equals("c1") && !this.args[1].equals("c2") && !this.args[1].equals("pos1") && !this.args[1].equals("pos2") && !this.args[1].equals("wand")) {
